0504cebea83423ad63c6111e896937358b555a97
alpay <calalli1903@gmail.com>
2023-08-13 23:36:23 +0400

    Initial Commit

---
+++ GHFollowers/GHFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersApp.swift
+//  GHFollowers
+//
+//  Created by Alpay Calalli on 13.08.23.
+//
+
+import SwiftUI
+
+@main
+struct GHFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
